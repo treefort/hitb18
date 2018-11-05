8b33ea67c05ead82f77519c3f807fa99e9e01b84
treefort <evanbooth@gmail.com>
2018-11-05 10:47:55 -0500

    removed unused attr

--- data/presentation-slides.js
+++ data/presentation-slides.js
@@ -2,11 +2,9 @@ import PresentationContent from 'data/presentation-content';
 
 const Slide = ({
   classes,
-  transitionCount,
 }, slideNum) => ({
   title: `Slide ${slideNum}`,
   classes,
-  transitionCount,
 });
 
 const PresentationSlides = PresentationContent.map(Slide);
